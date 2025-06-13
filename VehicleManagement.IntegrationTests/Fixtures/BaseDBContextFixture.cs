2d5c12479464da3d9a2d39fc3877730b4338af7f
behzad dara <Behzad.dara.99@gmail.com>
2025-06-14 01:36:59 +0330

    done

--- VehicleManagement.IntegrationTests/Fixtures/BaseDBContextFixture.cs
+++ VehicleManagement.IntegrationTests/Fixtures/BaseDBContextFixture.cs
@@ -5,24 +5,21 @@ namespace VehicleManagement.IntegrationTests.Fixtures;
 public abstract class BaseDBContextFixture<TDBContext> : IDisposable
     where TDBContext : DbContext
 {
-    private TDBContext? _db;
-
     public TDBContext Build(string name)
     {
         var optionBuilder = new DbContextOptionsBuilder<TDBContext>().UseInMemoryDatabase(name);
         var options = optionBuilder.Options;
 
-        _db = Build(options);
-        _db.Database.EnsureCreated();
+        var db = Build(options);
+        db.Database.EnsureCreated();
 
-        return _db;
+        return db;
     }
 
     protected abstract TDBContext Build(DbContextOptions<TDBContext> options);
 
     public void Dispose()
     {
-        _db?.Dispose();
         GC.SuppressFinalize(this);
     }
 }
